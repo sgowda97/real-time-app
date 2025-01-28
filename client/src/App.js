dd0f6d357093efb0d4147bb7cd82b143edba18be
Shreyas S <shreyassgowda97@gmail.com>
2025-01-28 15:18:57 +0530

    Slate.js client init

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CollaborationPage from "./pages/CollaborationPage";
+
+function App() {
+    return (
+        <div className="App">
+            <CollaborationPage />
+        </div>
+    );
+}
+
+export default App;
